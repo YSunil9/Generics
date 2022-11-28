54b0a959a974b2e5e5a75598c24cb3d8c47e21b8
Ysunil9 <sunil.pcs24@gmail.com>
2022-11-28 22:53:48 +0530

    Welcome to Generics Problem

--- Generics/FindMaxNum.cs
+++ Generics/FindMaxNum.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,7 @@ namespace Generics
 {
     internal class FindMaxNum
     {
-        public static void MaxValue(float first, float second, float third)
+        public static void MaxValue(string first, string second, string third)
         {
             if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
             {
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -1,11 +1,14 @@
-﻿namespace Generics
+﻿using System.IO;
+using System;
+
+namespace Generics
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Generics Problem");
-            FindMaxNum.MaxValue(90.22f, 20.111f, 10.2f);
+            FindMaxNum.MaxValue("Apple", "Peach", "Banana");
         }
     }
 }
