307630b6d1a729ee4e156e4fe85c15199ad289be
Ysunil9 <sunil.pcs24@gmail.com>
2022-11-29 22:50:55 +0530

    Refactor All

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -8,7 +8,10 @@ namespace Generics
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Generics Problem");
+            FindMaxNum.MaxValue(20,50,30);
+            FindMaxNum.MaxValue(90.22f, 20.111f, 10.2f);
             FindMaxNum.MaxValue("Apple", "Peach", "Banana");
+            Console.ReadLine();
         }
     }
 }
