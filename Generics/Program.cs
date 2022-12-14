d3ec149773d43df8ea195ac70cdae2459a1f8f7c
Ysunil9 <sunil.pcs24@gmail.com>
2022-12-14 16:17:51 +0530

    Welcome To UC4

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -5,13 +5,17 @@ namespace Generics
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
+            int[] array = { 4, 3, 2, 1 };
             Console.WriteLine("Welcome To Generics Problem");
             FindMaxNum.MaxValue(20,50,30);
             FindMaxNum.MaxValue(90.22f, 20.111f, 10.2f);
             FindMaxNum.MaxValue("Apple", "Peach", "Banana");
-            Console.ReadLine();
+
+            GenericsFindMax<int> genericsFindMax = new GenericsFindMax<int>();
+            genericsFindMax.MaxVal(array);
         }
     }
 }
